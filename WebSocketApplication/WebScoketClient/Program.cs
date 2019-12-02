472ecfcdcb91abfdf29cdf6505c32fa9476f3bf0
Juan Rivera <juanrivera2306@outlook.com>
2019-12-02 10:25:59 -0600

    Initial Project

---
+++ WebSocketApplication/WebScoketClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace WebScoketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("wss://localhost:44383/ws"))
+            {
+                ws.OnMessage += (sender, e) =>
+                Console.WriteLine("Received Message: " + e.Data);
+                ws.Connect();
+                ws.Send("Test Message");
+                Console.ReadKey();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
