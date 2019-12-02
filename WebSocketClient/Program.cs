dcfd9c5315b8782ad91c80e53ee91231eb997747
Juan Rivera <juanrivera2306@outlook.com>
2019-12-02 10:49:04 -0600

    Adding WebSocketClient

---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("wss://localhost:44357/ws"))
+            {
+                ws.OnMessage += (sender, e) =>
+                Console.WriteLine("Received Message: " + e.Data);
+                ws.Connect();
+                ws.Send("Test Message");
+                Console.ReadKey();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
